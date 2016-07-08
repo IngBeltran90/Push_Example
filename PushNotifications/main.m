cd9a603e690a03c9c69ed79c69f3b5109e0df04c
Philbert <philbert@philberts-mac.local>
2016-07-08 18:22:54 -0500

    Initial Commit

---
+++ PushNotifications/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PushNotifications
+//
+//  Created by Philbert on 7/8/16.
+//  Copyright © 2016 Philbert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
